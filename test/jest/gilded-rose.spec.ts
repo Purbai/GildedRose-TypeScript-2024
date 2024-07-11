36f52e319f0dda5b46dc26b24c386a21867e47e6
Natasha Buckham <natashaisobel@gmail.com>
2024-07-11 11:47:04 +0100

    Adds test case for simple items

--- test/jest/gilded-rose.spec.ts
+++ test/jest/gilded-rose.spec.ts
@@ -1,9 +1,15 @@
 import { Item, GildedRose } from '@/gilded-rose';
 
-describe('Gilded Rose', () => {
+describe.only('Gilded Rose', () => {
   it('should foo', () => {
     const gildedRose = new GildedRose([new Item('foo', 0, 0)]);
     const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe('fixme');
+    expect(items[0].name).toBe('foo');
+  });
+
+  it('should decrease quality by 1 for normal items', () => {
+    const gildedRose = new GildedRose([new Item("+5 Dexterity Vest", 10, 20)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(19);
   });
 });
