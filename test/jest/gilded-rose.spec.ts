d271e1ca71267d078fc5b47fb8b742df4f522094
Purbai <purbai.varsani@softwire.com>
2024-07-11 12:03:07 +0100

    add test for Brie

--- test/jest/gilded-rose.spec.ts
+++ test/jest/gilded-rose.spec.ts
@@ -12,4 +12,16 @@ describe.only('Gilded Rose', () => {
     const items = gildedRose.updateQuality();
     expect(items[0].quality).toBe(19);
   });
+
+  it('should decrease quality by 1 for Brie if quality is < 50', () => {
+    const gildedRose = new GildedRose([new Item("Aged Brie", 2, 0)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(1);
+  });
+  it('it should not increase quality for Brie over 50', () => {
+    const gildedRose = new GildedRose([new Item("Aged Brie", 2, 50)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(50);
+  });
+
 });
