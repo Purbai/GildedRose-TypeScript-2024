a84430b6a6723bf3b1aec768212b1b9162ccb708
Purbai <purbai.varsani@softwire.com>
2024-07-12 11:20:52 +0100

    added more tests

--- test/jest/gilded-rose.spec.ts
+++ test/jest/gilded-rose.spec.ts
@@ -53,13 +53,24 @@ describe('Gilded Rose', () => {
     const gildedRose = new GildedRose([new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49)]);
     const items = gildedRose.updateItem();
     expect(items[0].quality).toBe(50);
+    expect(items[0].sellIn).toBe(4);
   });
 
-  it('decrease the selln days by 1 if not Sulfuras', () => {
-    const gildedRose = new GildedRose([new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49)]);
+  
+  it('should reduce quality to zero  if selling days < 0 for Backstage', () => {
+    const gildedRose = new GildedRose([new Item("Backstage passes to a TAFKAL80ETC concert", -1, 20)]);
+    const items = gildedRose.updateItem();
+    expect(items[0].quality).toBe(0);
+    expect(items[0].sellIn).toBe(-2);
+  })
+
+
+  it('decrease the selln days by 1', () => {
+    const gildedRose = new GildedRose([new Item("someitem", 5, 49)]);
     const items = gildedRose.updateItem();
     expect(items[0].sellIn).toBe(4);
   });
+
 
   it('should not change the selln days if Sulfuras', () => {
     const gildedRose = new GildedRose([new Item("Sulfuras, Hand of Ragnaros", 0, 80)]);
@@ -79,10 +90,24 @@ describe('Gilded Rose', () => {
     expect(items[0].quality).toBe(22);
   })
   
-  it('should reduce quality to zero  if selling days < 0 for Backstage', () => {
-    const gildedRose = new GildedRose([new Item("Backstage passes to a TAFKAL80ETC concert", -1, 20)]);
+
+  it('should reduce quality by 2  if selling days = 0 for normal', () => {
+    const gildedRose = new GildedRose([new Item("someitem", 0, 20)]);
     const items = gildedRose.updateItem();
-    expect(items[0].quality).toBe(0);
+    expect(items[0].quality).toBe(18);
+  })
+
+  
+  it('should reduce quality by 2  if selling days = 1 for normal', () => {
+    const gildedRose = new GildedRose([new Item("someitem", 1, 20)]);
+    const items = gildedRose.updateItem();
+    expect(items[0].quality).toBe(19);
+  })
+
+  it('should reduce quality by 2  if selling days < 0 for normal', () => {
+    const gildedRose = new GildedRose([new Item("someitem", -1, 20)]);
+    const items = gildedRose.updateItem();
+    expect(items[0].quality).toBe(18);
   })
 
 });
